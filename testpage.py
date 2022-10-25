15a9efcb91489c4e8f09b69d30fba036b018734e
VadimZhuckow <97046345+VadimZhuckow@users.noreply.github.com>
2022-10-25 16:19:25 +0300

    Add files via upload

---
+++ testpage.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def main():
+    driver=webdriver.Chrome()
+    driver.get("Instagramhttps://www.instagram.com")
+    time.sleep(5)
+    el = driver.find_element(By.XPATH, "#")
+    el.click()
+    time.sleep(5)
+    eml = driver.find_element(By.XPATH,'#')
+    eml.send_keys('mail')
+    time.sleep(2)
+    btn_go = driver.find_element(By.XPATH,'#')
+    btn_go.click()
+    time.sleep(2)
+    pas_page = driver.find_element(By.XPATH,'#')
+    pas_page.send_keys('pass')
+    time.sleep(4)
+    
+    btn_send = driver.find_element(By.XPATH,'#')
+    btn_send.click()
+    time.sleep(2)
+    categ = driver.find_element(By.XPATH,'#')
+    categ.click()
+    time.sleep(1)
+    prof = driver.find_element(By.XPATH,'#')
+    prof.click()
+    time.sleep(1)
+    usermane = driver.find_element(By.XPATH,'#')
+    usermane.clear()
+    time.sleep(1)
+    usermane.send_keys("#")
+    time.sleep(1)
+    sohr = driver.find_element(By.XPATH,'#')
+    sohr.click()
+    time.sleep(5)
+    driver.quit()
+if __name__ == "__main__":
+    main()
